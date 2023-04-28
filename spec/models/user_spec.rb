eafa6dd42aa83bc06ee24037d1ed940f5c3646e7
kenzaki777 <128678672+kenzaki777@users.noreply.github.com>
2023-04-28 14:27:22 +0900

    ユーザー管理機能第１修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it '正常に登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録出来ない時' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -105,4 +111,5 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birth day can't be blank")
     end
   end
+end
 end
