0893aeaa964a41f9efd343798e9245c5ac85e45c
kenzaki777 <128678672+kenzaki777@users.noreply.github.com>
2023-05-01 16:29:31 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
   end
+  def new
+   @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
+
+private
+
+  def item_params
+    params.require(:item).permit(:item_name, :concept,:item_type_id,:item_condition_id,:price,:sender_area_id,:days_ship_id,:cost_type_id, :image).merge(user_id: current_user.id)
+  end
+  
