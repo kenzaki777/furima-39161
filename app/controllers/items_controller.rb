324927e0f3d4b8e71a82ad6b01c673036eb5a61b
kenzaki777 <128678672+kenzaki777@users.noreply.github.com>
2023-05-03 15:01:58 +0900

    商品削除機能第２修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit,:destroy]
   before_action :road_item,only:[:show,:edit,:update,:destroy]
-  before_action :judgment_user,only:[:edit,:update]
+  before_action :judgment_user,only:[:edit,:update,:destroy]
   def index
     @items = Item.all.order("id DESC")
   end
